1a2764951ab8be400c484be6933bd2f3ac2b1ac6
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-08-20 15:10:59 +0530

    added leetcode

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import LinkedinLogo from "../assets/LinkedIn.png";
 import XLogo from "../assets/logo-white.png";
 import GmailLogo from "../assets/gmail.png";
+import LeetcodeLogo from "../assets/leetcode.png"
 
 interface GitHubData {
   login: string;
@@ -70,12 +71,20 @@ export const Connect = () => {
           <img src={GmailLogo} alt="Gmail logo" className="h-10 w-auto" />
         </a>
         <a
+          href="https://leetcode.com/u/Sameer_Vohra/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={LeetcodeLogo} alt="Leetcode logo" className="h-10 w-auto" />
+        </a>
+        <a
           href="https://drive.google.com/uc?export=download&id=1ffIG7UJnrrNqJ1aUHdQ9kVw0K6AqYqGk"
           download
           className="bg-gray-600 hover:bg-cyan-500 text-white py-2 px-4 rounded-xl text-xl"
         >
           Download Resume
-        </a>{" "}
+        </a>
+       
       </div>
     </div>
   );
